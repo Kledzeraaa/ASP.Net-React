020ae4b9c2379386c44dff886330cb1ea78b310a
Clayton Santos <kledev02@hotmail.com>
2024-01-07 16:52:20 -0300

    Creating an Entity Framework Db Context

--- Reactivities/API/Controllers/WeatherForecastController.cs
+++ Reactivities/API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]")] //localhost:5000/weatherForecast
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
